aaed1d63b1a75819e7cc2a18fc88ff155b97cc72
Nashira Deer <velocicraft2088@gmail.com>
2022-07-13 18:56:09 -0300

    Add multilanguages support

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <QApplication>
+#include <QTranslator>
 #include "mainwindow.h"
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
+
+    QTranslator myappTranslator;
+    if (myappTranslator.load(QLocale::system(), "brasilcov", "_", ":/lang"))
+        app.installTranslator(&myappTranslator);
 
     MainWindow mainwindow;
     mainwindow.show();
