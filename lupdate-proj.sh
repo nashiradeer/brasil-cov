7d8276be5a3f7499feed21760bc2786c5ae323db
Nashira Deer <velocicraft2088@gmail.com>
2022-07-13 18:31:31 -0300

    Create a script to generate translations

---
+++ lupdate-proj.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+lupdate \
+    resources.qrc \
+    src/aboutwindow.ui src/aboutwindow.cpp \
+    src/mainwindow.cpp src/mainwindow.ui \
+    src/optionswindow.ui src/optionswindow.cpp \
+    src/statsitem.ui src/statsitem.ui \
+    src/statsmenu.cpp src/statsmenu.ui \
+    -ts lang/default.ts \
+    lang/portugues.ts
